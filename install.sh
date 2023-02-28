1f152a8b7c57cf5f2a329a27993c1f79a8098cd5
Fang <>
2023-02-28 19:58:38 +0800

    --wip-- [skip ci]

--- install.sh
+++ install.sh
@@ -6,6 +6,6 @@ sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
 curl -O https://gitlab.com/shardeum/validator/dashboard/-/raw/main/installer.sh && chmod +x installer.sh
-echo "y" | echo "y" | echo "wudi1234"| echo "" | echo ""| echo ""| echo ""|./installer.sh
+./installer.sh
 docker exec shardeum-dashboard /bin/bash -c "operator-cli gui start"
 
